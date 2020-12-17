01d7a13957d71145b7295d0deeb2e8a6a35c7311
kawashimakouhei <kk.1989.0722@gmail.com>
2020-12-18 01:47:13 +0900

    新規登録機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,4 +8,18 @@ class ApplicationController < ActionController::Base
       username == 'kouhei' && password == '7970'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_reading])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_reading])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthdate])
+
+  end
+
+  
+  
 end
+
