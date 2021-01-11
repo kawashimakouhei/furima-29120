7128d501923df3113b70168e088a009b413752ab
kawashimakouhei <kk.1989.0722@gmail.com>
2021-01-11 22:10:47 +0900

    商品保存実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :image, :text, :price, :category_id, :condition_id, :shipping_id, :prefecture_id, :day_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
