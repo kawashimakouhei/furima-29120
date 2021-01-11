aadd0bd2b49907c2e917755104909cbb74b592d2
kawashimakouhei <kk.1989.0722@gmail.com>
2021-01-11 22:11:59 +0900

    商品、ユーザーモデルにアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :day_to_ship
+  has_one_attached :image
+
+
+  validates :title, :text, :image, presence: true
+  validates :category_id, :condition_id, :shipping_id, :prefecture_id, :day_to_ship_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  belongs_to :user
 end
+
