1faf99a02be8c5567425f1f145189137296de9eb
kawashimakouhei <kk.1989.0722@gmail.com>
2021-01-12 21:29:26 +0900

    モデルバリテーション 修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day_to_ship
   has_one_attached :image
-
-
-  validates :title, :text, :image, presence: true
-  validates :category_id, :condition_id, :shipping_id, :prefecture_id, :day_to_ship_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
-  validates :price, format: { with: /\A[0-9]+\z/ }
   belongs_to :user
+
+  with_options presence: true do
+   validates :title, :text, :image, presence: true
+   validates :category_id, :condition_id, :shipping_id, :prefecture_id, :day_to_ship_id, numericality: {other_than: 1, message: "can't be blank"}
+   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
+   validates :price, format: { with: /\A[0-9]+\z/ }
+  end
+  
 end
 
