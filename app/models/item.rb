de7d8786c343d59ee91741e3ccfbc9e8bf152fd6
kawashimakouhei <kk.1989.0722@gmail.com>
2021-01-13 20:52:58 +0900

    バリテーション 修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   with_options presence: true do
    validates :title, :text, :image, presence: true
    validates :category_id, :condition_id, :shipping_id, :prefecture_id, :day_to_ship_id, numericality: {other_than: 1, message: "can't be blank"}
-   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99999999, message: "is invalid"}
+   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
    validates :price, format: { with: /\A[0-9]+\z/ }
   end
   
