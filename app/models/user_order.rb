2cce864be80ab5c0c3f68886e6440e1f2d3abb6a
kawashimakouhei <kk.1989.0722@gmail.com>
2021-02-05 21:02:28 +0900

    コメントアウトの文章消去

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -9,10 +9,12 @@ class UserOrder
     validates :house_number
     validates :token
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-  end
+    validates :item_id
+    validates :user_id
     validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+  end
+    
   def save
-    #Order.create(item_id: item_id, user_id: user_id)
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
   end
